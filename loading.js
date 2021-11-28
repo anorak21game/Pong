1435b2d128e41939ba3e4b13f2b34acc2869493f
Michael Wirth <mwirth@shiftphones.com>
2021-11-28 20:10:20 +0100

    style: clean up path

--- loading.js
+++ loading.js
@@ -3,7 +3,7 @@ function loadJSFile(folder, ...jsFile) {
     for (let index = 0; index < jsFile.length; index++) {
         let script = document.createElement("script");
         let className = jsFile[index];
-        let path = "../" + folder + "/" + className + ".js";
+        let path = folder + "/" + className + ".js";
         script.src = path;
         document.body.appendChild(script); 
     }
